023a3a395d47aa1d827ceb9c32a7b86ccb2f75f8
yacine bouklif <y.bouklif@it-students.fr>
2024-09-18 17:46:25 +0200

    Install Tailwind et son fichier config

---
+++ TailwindBoilerplate/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.{html,js}"],
+  theme: {
+    extend: {
+  colors : {
+    Macouleur: '#3490dc',
+    V:'#a855f7'
+  }
+    },
+  },
+  plugins: [],
+}
+
